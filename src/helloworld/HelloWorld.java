1485bccf83ddcd046f8dbea6733535443a497a56
Nahid Hasan <mrnahidhasan06@gmail.com>
2019-07-14 12:14:02 +0600

    show all student task completed

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package helloworld;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +13,32 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("1. Show all student");
+        System.out.println("2. Show random student");
+        System.out.println("3. Show number of  students");
+        System.out.println("4. add a student");
+        System.out.println("5. find a student");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int option =sc.nextInt();
+        
+        System.out.println("enter optionb (1-5):");
+        if(option==1){
+            
+        }
+       else if(option==2){
+            
+        }
+       else if(option==3){
+            
+        }
+       else if(option==4){
+            
+        }
+       else if(option==5){
+            
+        }
     }
     
 }
